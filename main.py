edd29dbb91de6acddc1fca574276edf43f7da4da
brightyorcerf <thetejaansh@gmail.com>
2026-02-18 02:53:21 +0530

    goodnight

--- main.py
+++ main.py
@@ -1,6 +1,32 @@
 from fastapi import FastAPI
-app = FastAPI()
+from typing import List, Dict, Any
+from datetime import datetime
+
+app = FastAPI(root_path="/api/v1")
+
+inventory: List[Dict[str, Any]] = [
+    {
+        "scoopID": "1",
+        "flavor": "Triple Fudge Brownie",
+        "churnedAt": datetime.now(),
+        "bestBefore": datetime.now()
+    }, 
+    {
+        "scoopID": "2",
+        "flavor": "Midnight Mint Chip",
+        "churnedAt": datetime.now(),
+        "bestBefore": datetime.now()
+    }
+]
 
 @app.get("/")
-async def  root():
-    return {"message" : "hello world"}
+async def welcomeToParlor():
+    return {"message": "welcome to the Ice Cream API!"}
+
+@app.get("/flavors")
+async def readFlavors(): 
+    return {"menu": inventory}
+
+@app.get("/flavors/{id}")
+async def readFlavor(id: int):
+    return {"menu": id}
